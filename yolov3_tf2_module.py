9f3f24221bc96591751e16cf75cb111e0e2a91a1
salim <salim.ahmed@ignitarium.com>
2020-11-30 14:30:12 +0530

    Modularized code into a class. Need to move yolo call to init, add only vehicle class functionality

--- yolov3_tf2_module.py
+++ yolov3_tf2_module.py
@@ -1,14 +1,15 @@
+# Author: Salim
 import os
+import cv2
+import numpy as np
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import tensorflow as tf
-from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_model, detect_video_realtime_mp, detect_vehicle
-from yolov3.configs import * #remove * imports
-
-image_path   = "./IMAGES/street.jpg"
-video_path   = "./IMAGES/test.mp4"
+from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_model, detect_video_realtime_mp, get_bounding_boxes
+from yolov3.configs import *     #remove * imports
 
 
-class vehicleDetector():
+
+class VehicleDetector():
 	def __init__(self, detect_classes=None):
 		'''
 		YOLOv3 TF2 module class for vehicle inference, training, crop. Supports object both normal and tiny implementations.
@@ -20,28 +21,39 @@ class vehicleDetector():
 		pass
 
 	def predict(self, image):
-		detections = self.yolo_obj.Predict(image)
-		out_boxes, out_scores, out_classes, classes, colors, __ = detections
-		return self.getVehicleCrop(image, out_boxes)
+		bboxes, image = get_bounding_boxes(self.yolo_obj, image_path, "./IMAGES/street_pred.jpg", input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
+		print(bboxes)
+		# out_boxes, out_scores, out_classes, classes, colors, __ = detections
+		return self.getVehicleCrop(image, bboxes)
 
-	def getVehicleCrop(self, image, out_boxes):
+	def getVehicleCrop(self, image, bboxes):
 		cropped = None
-		for bbox in out_boxes:
+		for i, bbox in enumerate(bboxes):
 			if bbox is not None:
-				x1, y1, x2, y2 = bbox 
+				coor = np.array(bbox[:4], dtype=np.int32)
+				score = bbox[4]
+				class_ind = int(bbox[5])
+				(x1, y1), (x2, y2) = (coor[0], coor[1]), (coor[2], coor[3])
 				cropped = image[y1:y2, x1:x2]
-				break
 		return cropped
 
-    def padding():
-        pass
+	@classmethod
+	def display_image(cls, cv_image, waitkey_param):
+		cv2.imshow('test', cv_image)
+		cv2.waitKey(waitkey_param)
+
+    # def padding():
+    #     pass
 
 if __name__=='__main__':
 	Available_vehicle_coco_classes = {'car':2, 'bus':5, 'truck':7}
 	
-	vehicle_obj = vehicleDetector([2, 5, 7])
-	image = cv2.imread('../testfiles/NTTE-CAM12-20200426_151_0125_73.jpg')
+	vehicle_obj = VehicleDetector([2, 5, 7])
+	image_path   = "./IMAGES/street.jpg"
+	video_path   = "./IMAGES/test.mp4"
+	image = cv2.imread(image_path)
 	cropped = vehicle_obj.predict(image)
 
-	#display_image(cropped, waitkey=0)
+	VehicleDetector.display_image(cropped, waitkey_param=0)
+
 	
