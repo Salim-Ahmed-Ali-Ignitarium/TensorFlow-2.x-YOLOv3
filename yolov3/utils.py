f0de50485a4a8f5639dae9dbc4e3dee24428b1d5
salim <salim.ahmed@ignitarium.com>
2020-11-30 20:28:55 +0530

    WORKING. Model is loaded on initialization itself. main method test cases added. time duration checks added. Optimizations might be required

--- yolov3/utils.py
+++ yolov3/utils.py
@@ -293,8 +293,6 @@ def detect_image(Yolo, image_path, output_path, input_size=416, show=False, CLAS
     bboxes = postprocess_boxes(pred_bbox, original_image, input_size, score_threshold)
     bboxes = nms(bboxes, iou_threshold, method='nms')
 
-    print(bboxes)
-
     image = draw_bbox(original_image, bboxes, CLASSES=CLASSES, rectangle_colors=rectangle_colors)
     # CreateXMLfile("XML_Detections", str(int(time.time())), original_image, bboxes, read_class_names(CLASSES))
 
@@ -558,73 +556,3 @@ def detect_realtime(Yolo, output_path, input_size=416, show=False, CLASSES=YOLO_
                 break
 
     cv2.destroyAllWindows()
-
-    
-#######################
-def get_bounding_boxes(Yolo, image_path, output_path, input_size=416, show=False, CLASSES=YOLO_COCO_CLASSES, score_threshold=0.3, iou_threshold=0.45, rectangle_colors=''):
-    original_image      = cv2.imread(image_path)
-    original_image      = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
-    original_image      = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
-
-    image_data = image_preprocess(np.copy(original_image), [input_size, input_size])
-    image_data = image_data[np.newaxis, ...].astype(np.float32)
-
-    if YOLO_FRAMEWORK == "tf":
-        pred_bbox = Yolo.predict(image_data)
-    elif YOLO_FRAMEWORK == "trt":
-        batched_input = tf.constant(image_data)
-        result = Yolo(batched_input)
-        pred_bbox = []
-        for key, value in result.items():
-            value = value.numpy()
-            pred_bbox.append(value)
-        
-    pred_bbox = [tf.reshape(x, (-1, tf.shape(x)[-1])) for x in pred_bbox]
-    pred_bbox = tf.concat(pred_bbox, axis=0)
-    
-    bboxes = postprocess_boxes(pred_bbox, original_image, input_size, score_threshold)
-    bboxes = nms(bboxes, iou_threshold, method='nms')
-
-    return bboxes, original_image
-    # image = draw_bbox(original_image, bboxes, CLASSES=CLASSES, rectangle_colors=rectangle_colors)
-    # # CreateXMLfile("XML_Detections", str(int(time.time())), original_image, bboxes, read_class_names(CLASSES))
-
-    # if output_path != '': cv2.imwrite(output_path, image)
-    # if show:
-    #     # Show the image
-    #     cv2.imshow("predicted image", image)
-    #     # Load and hold the image
-    #     cv2.waitKey(0)
-    #     # To close the window after the required kill value was provided
-    #     cv2.destroyAllWindows()
-        
-    # return image
-
-
-def draw_bbox(image, bboxes, CLASSES=YOLO_COCO_CLASSES, show_label=True, show_confidence = True, Text_colors=(255,255,0), rectangle_colors='', tracking=False):   
-    NUM_CLASS = read_class_names(CLASSES)
-    num_classes = len(NUM_CLASS)
-    image_h, image_w, _ = image.shape
-    hsv_tuples = [(1.0 * x / num_classes, 1., 1.) for x in range(num_classes)]
-    #print("hsv_tuples", hsv_tuples)
-    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
-    colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
-
-    random.seed(0)
-    random.shuffle(colors)
-    random.seed(None)
-
-    for i, bbox in enumerate(bboxes):
-        coor = np.array(bbox[:4], dtype=np.int32)
-        score = bbox[4]
-        class_ind = int(bbox[5])
-        bbox_color = rectangle_colors if rectangle_colors != '' else colors[class_ind]
-        bbox_thick = int(0.6 * (image_h + image_w) / 1000)
-        if bbox_thick < 1: bbox_thick = 1
-        fontScale = 0.75 * bbox_thick
-        (x1, y1), (x2, y2) = (coor[0], coor[1]), (coor[2], coor[3])
-
-        # put object rectangle
-        cv2.rectangle(image, (x1, y1), (x2, y2), bbox_color, bbox_thick*2)
-
-    return image
--- yolov3_tf2_module.py
+++ yolov3_tf2_module.py
@@ -2,31 +2,62 @@
 import os
 import cv2
 import numpy as np
+import time
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import tensorflow as tf
-from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_model, detect_video_realtime_mp, get_bounding_boxes
+from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_model, detect_video_realtime_mp, image_preprocess, postprocess_boxes, nms
 from yolov3.configs import *     #remove * imports
 
 
 
 class VehicleDetector():
+	yolo_obj = ""
 	def __init__(self, detect_classes=None):
 		'''
 		YOLOv3 TF2 module class for vehicle inference, training, crop. Supports object both normal and tiny implementations.
+		TO DO: 1.add tiny and normal implementations
+				2. add choice of detect_classes
 		'''
-		self.detect_classes = detect_classes # None, #[5, 7] refer coco_classes.txt 'yolo/pretrained_weights/coco_classes.txt' for details
-		self.yolo_obj = Load_Yolo_model()
+		detect_classes = detect_classes 
+		VehicleDetector.yolo_obj = Load_Yolo_model()
 	
 	def train(self):
 		pass
 
-	def predict(self, image):
-		bboxes, image = get_bounding_boxes(self.yolo_obj, image_path, "./IMAGES/street_pred.jpg", input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
-		print(bboxes)
+	def predict(self, image_path, pred_result_path):
+		bboxes, image = VehicleDetector.get_bounding_boxes(image_path, pred_result_path, input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
+		# print(bboxes)
 		# out_boxes, out_scores, out_classes, classes, colors, __ = detections
-		return self.getVehicleCrop(image, bboxes)
+		return self.get_vehicle_crop(image, bboxes)
 
-	def getVehicleCrop(self, image, bboxes):
+
+	def get_bounding_boxes(image_path, output_path, input_size=416, show=False, CLASSES=YOLO_COCO_CLASSES, score_threshold=0.3, iou_threshold=0.45, rectangle_colors=''):
+		original_image      = cv2.imread(image_path)
+		original_image      = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+		original_image      = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+
+		image_data = image_preprocess(np.copy(original_image), [input_size, input_size])
+		image_data = image_data[np.newaxis, ...].astype(np.float32)
+
+		if YOLO_FRAMEWORK == "tf":
+			pred_bbox = VehicleDetector.yolo_obj.predict(image_data)
+		elif YOLO_FRAMEWORK == "trt":
+			batched_input = tf.constant(image_data)
+			result = VehicleDetector.yolo_obj(batched_input)
+			pred_bbox = []
+			for key, value in result.items():
+				value = value.numpy()
+				pred_bbox.append(value)
+			
+		pred_bbox = [tf.reshape(x, (-1, tf.shape(x)[-1])) for x in pred_bbox]
+		pred_bbox = tf.concat(pred_bbox, axis=0)
+		
+		bboxes = postprocess_boxes(pred_bbox, original_image, input_size, score_threshold)
+		bboxes = nms(bboxes, iou_threshold, method='nms')
+		
+		return bboxes, original_image
+
+	def get_vehicle_crop(self, image, bboxes):
 		cropped = None
 		for i, bbox in enumerate(bboxes):
 			if bbox is not None:
@@ -38,22 +69,34 @@ class VehicleDetector():
 		return cropped
 
 	@classmethod
-	def display_image(cls, cv_image, waitkey_param):
+	def display_image(cls, cv_image, waitkey_param=0):
 		cv2.imshow('test', cv_image)
 		cv2.waitKey(waitkey_param)
-
+		cv2.destroyAllWindows()	
+		
     # def padding():
     #     pass
 
 if __name__=='__main__':
 	Available_vehicle_coco_classes = {'car':2, 'bus':5, 'truck':7}
-	
 	vehicle_obj = VehicleDetector([2, 5, 7])
+	
 	image_path   = "./IMAGES/street.jpg"
-	video_path   = "./IMAGES/test.mp4"
-	image = cv2.imread(image_path)
-	cropped = vehicle_obj.predict(image)
+	pred_result_path = "./IMAGES/street_pred.jpg"
+	# video_path   = "./IMAGES/test.mp4"
+	
+	# test image 1
+	cropped = vehicle_obj.predict(image_path, pred_result_path)
+	VehicleDetector.display_image(cropped, 0)
 
-	VehicleDetector.display_image(cropped, waitkey_param=0)
+	# test image 2 with time check
+	t1 = time.time()
+	image_path = "./IMAGES/city.jpg"
+	pred_result_path = "./IMAGES/city_pred.jpg"
+	cropped = vehicle_obj.predict(image_path, pred_result_path)
+	# VehicleDetector.display_image(cropped, 0)
+	t2 = time.time()
+
+	print("TIME", t2-t1)
 
 	
